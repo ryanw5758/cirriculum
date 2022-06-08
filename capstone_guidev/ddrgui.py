24bf70b96a7367cbd20441aef598006401ee620e
Ryan White <ryanw5758@gmail.com>
2022-06-08 12:53:52 -0400

    Add matrix features, main menu traversal
    Rev 1.3: Functionality to return to main menu, matrix representation with Canvas widget on gameLoop screen, handling for destruction/creation of matrix. Added sample avatar pictures to simplify avatar select screen development.

--- capstone_guidev/ddrgui.py
+++ capstone_guidev/ddrgui.py
@@ -1,34 +1,95 @@
-# Main UI
-# Needs to: run on Raspberry Pi power on (exec UNIX command), take button input from several different I/O pins (? depends on how we implement), access and discriminate between each pin,
-# change color depending on what button was pressed
+# Main UI REV 1.3
+# 1.3 Changes: functionality to return to main menu, matrix representation with Canvas widget on gameLoop screen, handling for destruction/creation of matrix
+# refactor into OOP approach: more readable and easier to edit frames between functions
 
-# for now, just changes color based on button input, button needs to execute a C++/C file in the final version of product
+# TODO Next Revision: displaying avatars on avatar select screen (store path in variable most likely)
+# refactor into OOP approach: more readable and easier to edit frames between functions
 
 import tkinter as tk
+from tkinter import Canvas, Frame
 
-def ChangeRed():
-    print("Red button pressed")
-    root.configure(bg = 'red')
-
-def ChangeBlue():
-    print("Blue button pressed")
-    root.configure(bg = 'blue')
-
-def ChangeYellow():
-    print("Yellow button pressed")
-    root.configure(bg = 'yellow')
-
+# create root and frames globally to ensure functions can access data
 root = tk.Tk()
+mainMenu = Frame(root)
+avatarSelect = Frame(root)
+gameScreen = Frame(root)
 
-root.title('DDR')
-root.geometry('400x400') # in final version, needs to be fixed to size of monitor, borderless, etc.
+def LaunchAvatar():
+    print("Entered Avatar Select")
+    avatarSelect.pack(fill='both', expand=1)
+    mainMenu.pack_forget()
+    gameScreen.pack_forget()
 
-redButton = tk.Button(root, text = "Red", command = lambda:ChangeRed())
-blueButton = tk.Button(root, text = "Blue", command = lambda:ChangeBlue())
-yellowButton = tk.Button(root, text = "Yellow", command = lambda:ChangeYellow())
+def LaunchQP():
+    print("Entered Quick Play")
+    gameScreen.pack(fill='both', expand=1)
+    LoadMatrix()
+    mainMenu.pack_forget()
+    avatarSelect.pack_forget()
 
-redButton.pack()
-blueButton.pack()
-yellowButton.pack()
+def LaunchMM():
+    print("Entered mainMenu")
+    mainMenu.pack(fill='both', expand=1)
+    avatarSelect.pack_forget()
 
-root.mainloop()
+    # delete only the Canvas widget: deleting the rest blows up the whole GUI since frame is only initialized once
+    for widget in gameScreen.winfo_children():
+        if (type(widget) == Canvas):
+            widget.destroy()
+            break
+
+    gameScreen.pack_forget()
+
+def LoadMatrix():
+    xIter = 0; yIter = 0
+    matrix = tk.Canvas(gameScreen, bg="white", height=300, width=300)
+
+    # probably not efficient: refactor in next revision
+    # doesn't matter what color the squares are for now: data from the MCU will decide that in a later revision
+    for i in range(9):
+        matrix.create_rectangle(xIter, yIter, xIter+100, yIter+100, fill='red')
+        if xIter == 200:
+            yIter += 100
+            xIter = 0
+        else:
+            xIter += 100
+    matrix.pack()
+
+def main():
+
+    root.title('Main Menu')
+    root.geometry('400x400') # in final version, needs to be fixed to size of monitor, borderless, etc.
+
+    # initialize frame displays
+    labelGS = tk.Label(gameScreen, text="Displaying game screen", font="Helvetica")
+    labelGS.pack()
+    labelAvatar = tk.Label(avatarSelect, text="Displaying avatar select", font="Helvetica")
+    labelAvatar.pack()
+
+    returnMM_GS = tk.Button(gameScreen, text = "Return to Main Menu", command = lambda:LaunchMM())
+    returnMM_GS.pack()
+    returnMM_AS = tk.Button(avatarSelect, text = "Return to Main Menu", command = lambda:LaunchMM())
+    returnMM_AS.pack()
+
+    # title: thinking about making something in GIMP or photoshop instead of just a plain label
+    titleVar = tk.StringVar()
+    mmTitle = tk.Label(mainMenu, textvariable=titleVar, font="Helvetica", relief=tk.RAISED)
+    titleVar.set("Ortho-Matryx")
+
+    avatarButton = tk.Button(mainMenu, text = "Select Avatar", command = lambda:LaunchAvatar())
+    qpButton = tk.Button(mainMenu, text = "Quick Play", command = lambda:LaunchQP())
+
+    # load mainMenu frame
+    mainMenu.pack(fill='both', expand=1)
+
+    mmTitle.place(relx=0.5, rely = 0.4, anchor = tk.CENTER)
+    avatarButton.place(relx=0.5, rely = 0.5, anchor = tk.CENTER)
+    qpButton.place(relx=0.5, rely = 0.58, anchor = tk.CENTER)
+
+    # makes gui borderless: don't do this yet since it makes development harder
+    #root.overrideredirect(True)
+    
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
